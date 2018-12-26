b012fc5a14baa8145884ec25deed442492975daa
williamisfranciscodasilva <williamisepaula@gmail.com>
2018-12-26 12:51:13 -0200

    Programa em Java que mostra o maior valor entre dois valores, usando operador ternario

---
+++ maior2.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+class maior2
+{
+    public static void main(String args[])
+    {
+        Float num1, num2, mnum;
+
+        num1 =Float.parseFloat(
+        JOptionPane.showInputDialog(""));
+        num2 =Float.parseFloat(
+        JOptionPane.showInputDialog(""));
+
+        mnum = (num1 > num2) ? num1 : num2;
+        JOptionPane.showMessageDialog(null,"O maior valor entre "+num1+" e "+num2+" é: "+mnum);
+    }
+}
